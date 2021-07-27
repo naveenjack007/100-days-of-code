587f6ce6e5b6ee33be74bdc7a02cc5a7f5a8152a
Naveen Natarajan <58937732+naveenjack007@users.noreply.github.com>
2021-07-27 22:17:06 +0530

    Day 1 of 100

---
+++ Day 1/Welcome.java
@@ -0,0 +1,9 @@
+//import java.util.*;
+
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+    
+}
