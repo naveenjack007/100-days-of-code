6e0b4ae74106105deda1ed341d9aeb4f12780872
Naveen Natarajan <58937732+naveenjack007@users.noreply.github.com>
2021-08-01 16:42:15 +0530

    Day 7 of 100

---
+++ Day 7/Character.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+//package Day 7;
+
+public class Character {
+    public static void main(String[] args) {
+        java.lang.String c;
+        Scanner input = new Scanner(System.in);
+        c = input.next();
+        input.close();
+        System.out.println(c);
+    }
+    
+}
